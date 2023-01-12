4b674d89eb092bec06718cc5732ce226faa865bf
jabedabedo <suryajabbar3@gmail.com>
2023-01-12 14:12:04 +0700

    add perulangan while.py

---
+++ while.py
@@ -0,0 +1,11 @@
+jumlah_buku = 10
+print('ibu berkata, "baca semua bukumu !"')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f'jumlah buku yang sudah dibaca = {jumlah_buku_yang_sudah_dibaca}')
+
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
+    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
+    print(f"Buku Ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
